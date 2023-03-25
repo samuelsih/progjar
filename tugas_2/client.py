7b4d5ebc3971c09a60166f2856accad97aac34bd
samuelsih <samuel.205025@mhs.its.ac.id>
2023-03-26 06:38:25 +0700

    add tugas 2

---
+++ tugas_2/client.py
@@ -0,0 +1,32 @@
+import socket
+import logging
+
+def kirim_data():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    logging.warning("membuka socket")
+
+    server_address = ('ff0c37cc19ca', 45000)
+    logging.warning(f"opening socket {server_address}")
+    sock.connect(server_address)
+
+    try:
+        # Send data
+        message = 'TIME ini_msg' + chr(13) + chr(10)
+        logging.warning(f"[CLIENT] sending {message}")
+        sock.sendall(message.encode())
+        # Look for the response
+        amount_received = 0
+        amount_expected = len(message)
+        while amount_received < amount_expected:
+            data = sock.recv(16)
+            amount_received += len(data)
+            logging.warning(f"[DITERIMA DARI SERVER] {data}")
+    finally:
+        logging.warning("closing")
+        sock.close()
+    return
+
+
+if __name__=='__main__':
+    for i in range(1,10):
+        kirim_data()
