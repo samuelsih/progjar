2378edbbe25337d1d05f3d7359ee09bd703d40b6
samuelsih <samuel.205025@mhs.its.ac.id>
2023-03-26 13:02:59 +0700

    client_process_benchmark

---
+++ tugas_3/client_process_benchmark.py
@@ -0,0 +1,25 @@
+from multiprocessing import Process
+from counter import AtomicCounter
+from task import kirim_data
+import datetime
+
+maxsize = 2044
+
+def client_process():
+    start = datetime.datetime.now()
+    counter = AtomicCounter()
+    
+    for i in range(maxsize):        
+        processor = Process(target=kirim_data)
+        processor.start()
+        processor.join()
+        
+        counter.increment()
+        
+    end  = datetime.datetime.now() - start
+    
+    print(f"Request total {counter.value()}")
+    print(f"Waktu TOTAL yang dibutuhkan {end} detik {start}")
+
+if __name__ == "__main__":
+    client_process()
